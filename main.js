a7042b3898d6bca38d0c01257064802892650794
Artyom Lyakhovsky <lyaxovskij1995@gmail.com>
2024-09-30 17:12:57 +0300

    Start

--- main.js
+++ main.js
@@ -5,23 +5,33 @@ const btn = document.querySelector('.add-block__btn-add')
 btn.addEventListener('click',  addItem)
 
 function addItem() {
-    const title = input.value
-    console.log(title)
-
-    const item = `<li class="list__item" data id="${new Date().getTime()}">
+    const title = input.value.trim()
+    const item = `<li class="list__item" data-id="${new Date().getTime()}">
     <div class="list__item-strip">
     <h1 class="list__item-title">${title}</h1>
-    <button class="list__item-btn">Del</button>
+    <button class="list__item-btn">&#128722</button>
     </div>
 </li>`
 
+ if (title === '') {
+     return false
+ } else {
+   
+ }
+   input.value = ''
 listItem.insertAdjacentHTML('afterbegin', item)
 
 
 const itemDelete = document.querySelector('.list__item-btn')
+itemDelete.addEventListener('click', function() {
+    const id = this.parentNode.parentNode.dataset.id
 
-itemDelete.addEventListener('click', function () {
-    
+    deletListItem(id)
 })
 
+}
+
+function deletListItem(id) {
+    const li = document.querySelectorAll('li')
+    li.forEach(el => el.dataset.id === id && el.remove())
 }
